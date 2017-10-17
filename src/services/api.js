14f0bae98ae9fb876d0a43177e779e489bfd8fe4
Greg Nitsenko <gregory.nitsenko@gmail.com>
2017-10-17 11:24:39 +0300

    adding stubs for api and sagas

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,9 @@
-export const callApi = (uri, method, body) => { } ;
+import 'isomorphic-fetch';
+
+const base = 'http://localhost:665/v1';
+
+export const user = (method, body) => fetch(`${base}/users`, { method, body });
+
+export const place = (method, body) => fetch(`${base}/place`, { method, body });
+
+export const bookings = (method, body) => fetch(`${base}/bookings`, { method, body });
