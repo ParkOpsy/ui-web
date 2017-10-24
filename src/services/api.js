ad562bb0732ac181ebc7d303e86b51ce8ac6be0c
Greg Nitsenko <gregory.nitsenko@nordigy.ru>
2017-10-24 17:37:42 +0300

    updating api

--- src/services/api.js
+++ src/services/api.js
@@ -2,19 +2,21 @@ import 'isomorphic-fetch';
 
 const base = 'http://api/v1';
 
-export const getUsers = () => fetch(`${base}/users`, { method: 'GET' });
-export const createUser = user => fetch(`${base}/users`, { method: 'POST', body: user });
+export const auth = () => fetch(`${base}/auth`, { method: 'GET'});
 
-export const getUser = id => fetch(`${base}/users/${id}`, { method: 'GET' });
-export const updateUser = (id, update) => fetch(`${base}/users/${id}`, { method:'PUT', body: update });
-export const removeUser = id => fetch(`${base}/users/${id}`, { method: 'DELETE' });
+export const getUsers = () => fetch(`${base}/user`, { method: 'GET' });
+export const createUser = user => fetch(`${base}/user`, { method: 'POST', body: user });
 
-export const getPlaces = () => fetch(`${base}/places`, { method: 'GET' });
-export const createPlace = place => fetch(`${base}/places`, { method: 'POST', body: place });
+export const getUser = userId => fetch(`${base}/user/${userId}`, { method: 'GET' });
+export const updateUser = (userId, update) => fetch(`${base}/user/${userId}`, { method:'PUT', body: { user: update }});
+export const removeUser = userId => fetch(`${base}/user/${userId}`, { method: 'DELETE' });
 
-export const getPlace = id => fetch(`${base}/places/${id}`, { method: 'GET' });
-export const updatePlace = (id, update) => fetch(`${base}/places/${id}`, { method: 'PUT', body: update });
-export const removePlace = id => fetch(`${base}/places/${id}`, { method: 'DELETE' });
+export const getPlaces = () => fetch(`${base}/place`, { method: 'GET' });
+export const createPlace = place => fetch(`${base}/place`, { method: 'POST', body: place });
 
-export const createBooking = (placeId, hostId) => fetch(`${base}/bookings/${placeId}`, { method: 'POST', body: { hostId } });
-export const cancelBooking = placeId => fetch(`${base}/bookings/${placeId}`, { method: 'DELETE' });
+export const getPlace = placeId => fetch(`${base}/place/${placeId}`, { method: 'GET' });
+export const updatePlace = (placeId, update) => fetch(`${base}/place/${placeId}`, { method: 'PUT', body: { place: update } });
+export const removePlace = placeId => fetch(`${base}/place/${placeId}`, { method: 'DELETE' });
+
+export const createBooking = (placeId, booking) => fetch(`${base}/place/${placeId}/booking`, { method: 'POST', body: { booking } });
+export const cancelBooking = (placeId, bookingId) => fetch(`${base}/bookings/${placeId}/booking/${bookingId}`, { method: 'DELETE' });
